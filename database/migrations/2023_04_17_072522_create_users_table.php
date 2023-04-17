71de84ee21a2439f1de67722bd65c91c7412c4fa
Alejandro Barrionuevo Guerrero <alejandrobarrionuevo00@gmail.com>
2023-04-17 09:38:23 +0200

    first commit

---
+++ database/migrations/2023_04_17_072522_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('role')->nullable();
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+            $table->string('nick')->nullable();
+            $table->string('email')->nullable();
+            $table->string('password')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+            $table->rememberToken();
+            $table->string('rember_token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Image;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $images = Image::all();
+    foreach($images as $image){
+        echo $image->image_path."<br>";
+        echo $image->description."<br>";
+    }
+    die();
+    
+    return view('welcome');
+});
